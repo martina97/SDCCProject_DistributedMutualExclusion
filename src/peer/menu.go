29c5951cc9d017dc358d00491841f49f5908a615
martina97 <martinadem97@gmail.com>
2022-11-17 12:36:07 +0100

    errore apertura file peer receiver 2

--- src/peer/menu.go
+++ src/peer/menu.go
@@ -40,6 +40,7 @@ func openMenu() {
 		if i+1 == 3 { //exit
 			algorithm = "RicartAgrawala"
 			setAlgorithmPeer()
+			setPeerUtils2()
 			openSecondMenu()
 
 		}
@@ -55,7 +56,6 @@ func openSecondMenu() {
 
 	// una volta scelto l'algoritmo, setto le info dei vari peer (in particolare il file di log, il cui path
 	// dipende dall0'algoritmo scelto
-	setPeerUtils2()
 
 	fmt.Println(" sto in openSecondMenu ------ ")
 	for { //infinite loop
