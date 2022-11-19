72d44cb32f1a5ee757ffb1e9213d68f85b955cc1
martina97 <martinadem97@gmail.com>
2022-11-19 11:26:58 +0100

    entro in CS ricart2

--- src/peer/RicartAgrawala/sender.go
+++ src/peer/RicartAgrawala/sender.go
@@ -67,8 +67,8 @@ func SendRicart(peer *RApeer) {
 	fmt.Println("IL MESSAGGIO E' ====", msg.ToString("send"))
 	sendRequest(msg)
 
-	MyRApeer.mutex.Unlock()
 	utilities.WriteInfoToFile2(MyRApeer.Username, MyRApeer.LogPath, "wait all peer reply messages.", false)
+	MyRApeer.mutex.Unlock()
 
 	//4. Wait until #replies=N-1;
 	<-MyRApeer.ChanAcquireLock
