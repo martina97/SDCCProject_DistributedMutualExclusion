d744c2828c229bc55174bd03f980e5ee316e3701
martina97 <martinadem97@gmail.com>
2022-11-23 17:03:38 +0100

    test - token asking

--- src/peer/peerTest.go
+++ src/peer/peerTest.go
@@ -3,11 +3,18 @@ package main
 import (
 	"fmt"
 	"log"
+	"time"
 )
 
 func startTests() {
 
-	runTest(1, "TokenAsking") //invia msg solo il peer con ID 1
+	/*
+		runTest(1, "tokenAsking") //invia msg solo il peer con ID 1
+		fmt.Println("runTest1 finito")
+
+	*/
+	runTest(2, "tokenAsking") //invia msg solo il peer con ID 1
+
 }
 
 func runTest(i int, s string) {
@@ -18,14 +25,29 @@ func runTest(i int, s string) {
 	algorithm = "tokenAsking"
 	setAlgorithmPeer()
 	fmt.Println(myTokenPeer)
-	if myTokenPeer.Username == "p1" {
-		err := sendMessage()
-		if err != nil {
-			log.Fatalf("error sending request %v", err)
+
+	if i == 1 {
+		if myTokenPeer.Username == "p2" {
+			err := sendMessage()
+			if err != nil {
+				log.Fatalf("error sending request %v", err)
+			}
+		} else {
+			//time.Sleep(time.Minute / 2)
+			//select {}
+			time.Sleep(time.Minute)
 		}
 	} else {
-		//time.Sleep(time.Minute / 2)
-		select {}
+		if myTokenPeer.Username == "p1" || myTokenPeer.Username == "p2" {
+			err := sendMessage()
+			if err != nil {
+				log.Fatalf("error sending request %v", err)
+			}
+		} else {
+			//time.Sleep(time.Minute / 2)
+			//select {}
+			time.Sleep(time.Minute + time.Minute/2)
+		}
 	}
 
 }
