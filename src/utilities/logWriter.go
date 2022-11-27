bfa5ae4381953f3a38815c391ea77451a18a000e
martina97 <martinadem97@gmail.com>
2022-11-27 12:41:22 +0100

    logWriter file -4

--- src/utilities/logWriter.go
+++ src/utilities/logWriter.go
@@ -26,28 +26,3 @@ func openFile(path string) *os.File {
 	}
 	return f
 }
-
-func WriteVCInfoToFile(path string, isCoord bool) {
-	var vc VectorClock
-	var username string
-
-	f := openFile(path)
-
-	if isCoord {
-		vc = myCoordinator.VC
-		username = "coordinator"
-	} else {
-		vc = myPeer.VC
-		username = myPeer.Username
-	}
-
-	//save new address on file
-	date := time.Now().Format(utilities.DATE_FORMAT)
-
-	_, err := f.WriteString("[" + date + "] : " + username + " update its vector clock to " + ToString(vc))
-	_, err = f.WriteString("\n")
-	err = f.Sync()
-	if err != nil {
-		log.Fatalf("error writing file: %v", err)
-	}
-}
